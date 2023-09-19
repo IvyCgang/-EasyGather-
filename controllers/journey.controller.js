60bd56ebb6c42e99a170ad3bac995c54a7751b8e
Ching Yi, Chan <qrtt1@infuseai.io>
2023-09-20 00:12:39 +0800

    update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- controllers/journey.controller.js
+++ controllers/journey.controller.js
@@ -1,35 +1,47 @@
-const express = require('express'),
-    router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 const service = require('../services/journey.service')
 
 
+router.post('/xxx', (req, res) => {
+    // Access the JSON data from the request body
+    const jsonData = req.body;
+
+    // You can now work with the JSON data as needed
+    console.log('Received JSON data:', jsonData);
+
+    // Respond with a success message
+    res.json({message: 'JSON data received successfully'});
+});
+
+
 //http://163.22.17.145:3000/api/journey/
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     const journey = await service.getAllJourney()
     res.send(journey)
 })
 
 
-router.get('/:jID', async(req, res) => {
+router.get('/:jID', async (req, res) => {
     const journey = await service.getJourneyById(req.params.jID)
-	if (journey.length == 0)
-		res.status(404).json('no record with jID: ' + req.params.jID)
-	else
-    res.send(journey)
+    if (journey.length == 0)
+        res.status(404).json('no record with jID: ' + req.params.jID)
+    else
+        res.send(journey)
 })
 
-router.delete('/:jID', async(req, res) => {
+router.delete('/:jID', async (req, res) => {
     const affectedRows = await service.deleteJourney(req.params.jID)
-        if (affectedRows == 0)
-                res.status(404).json('no record with jID: ' + req.params.jID)
-        else
-    res.send('delete successfully.')
+    if (affectedRows == 0)
+        res.status(404).json('no record with jID: ' + req.params.jID)
+    else
+        res.send('delete successfully.')
 })
 
 
-
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
+    console.log(req.body);
     await service.addJourney(req.body)
     res.status(201).send('add successfully.')
 })
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyparser = require('body-parser');
 require('express-async-errors')
 
 const db = require('./db'),
-	journeyRoutes = require('./controllers/journey.controller')
+    journeyRoutes = require('./controllers/journey.controller')
 
 //middlewore
 app.use(bodyparser.json())
@@ -17,15 +17,30 @@ app.use((err, req, res, next) => {
 })
 
 
-//first make sure db connected is ok
-//then start the express server
-db.query("SELECT 1")
-        .then(() => {
-            console.log('database connected')
-            app.listen(3000,
-                () => console.log('server started at 3000'))
+app.post('/api/receive-json', (req, res) => {
+    // Access the JSON data from the request body
+    const jsonData = req.body;
 
-            })
-        .catch(err => console.log('database connected failed'))
+    // You can now work with the JSON data as needed
+    console.log('Received JSON data:', jsonData);
 
+    // Respond with a success message
+    res.json({message: 'JSON data received successfully'});
+});
+
+// //first make sure db connected is ok
+// //then start the express server
+// db.query("SELECT 1")
+//         .then(() => {
+//             console.log('database connected')
+//             app.listen(3000,
+//                 () => console.log('server started at 3000'))
+//
+//             })
+//         .catch(err => console.log('database connected failed'))
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
