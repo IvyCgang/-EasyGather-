bf60c54d29aacadb1b763d6d47a7ca360fb8ff82
Your Name <you@example.com>
2023-09-20 02:20:17 +0800

    res

--- services/journey.service.js
+++ services/journey.service.js
@@ -17,7 +17,20 @@ module.exports.deleteJourney = async (jID) => {
 
 module.exports.addJourney = async (obj) => {
 	console.log(obj);
-        const [{affectedRows}] = await db.query("INSERT INTO journey (uID, journeyName, journeyStartTime, journeyEndTime, isAllDay, location, remindStatus, remindTime, remark, color) VALUES (?,?,?,?,?,?,?,?,?,?)",
+        
+		const [{affectedRows}] = await db.query("INSERT INTO journey (uID, journeyName, journeyStartTime, journeyEndTime, isAllDay, location, remindStatus, remindTime, remark, color) VALUES (?,?,?,?,?,?,?,?,?,?)",
 		[obj.uID, obj.journeyName, obj.journeyStartTime, obj.journeyEndTime, obj.isAllDay, obj.location, obj.remindStatus, obj.remindTime, obj.remark, obj.color])
-        return affectedRows;
-}
+        
+	//	const response = {
+         //  message: '新增成功',
+           // affectedRows: affectedRows
+        //};
+
+       // return response;
+   // } catch (error) {
+        // 如果有錯誤，建立 JSON 錯誤回應物件
+     //   const errorResponse = {
+       //     error: '發生錯誤'
+       // };
+		return affectedRows;
+};
