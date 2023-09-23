3348ee3efc8e016c8580fe08409cf3c423f2db28
Ching Yi, Chan <qrtt1@infuseai.io>
2023-09-23 19:54:32 +0800

    add debug logs
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- db.js
+++ db.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2/promise')
+const mysql = require("mysql2/promise");
+
+function logSqlQuery(query) {
+  console.log("Executing SQL query:", query);
+}
 
 const mysqlPool = mysql.createPool({
-	host: 'localhost',
-  	user: 'root',
-  	password: 'program135',
-  	database: 'program'
-})
+  host: "localhost",
+  user: "root",
+  password: "program135",
+  database: "program",
+  queryFormat: function (query, values) {
+    if (!values) return query;
+    return query.replace(/\?/g, (match) => {
+      if (values.length === 0) return match;
+      return mysql.escape(values.shift());
+    });
+  },
+  debug: logSqlQuery, // Set the custom logging function for queries
+});
 
-module.exports = mysqlPool
+module.exports = mysqlPool;
